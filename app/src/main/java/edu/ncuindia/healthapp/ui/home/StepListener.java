a08dcda324d8fe6242932f42bddaf5d17c7be0f1
nishantkhatri29 <nishantkhatri29@gmail.com>
2019-11-07 23:18:39 +0530

    Pedometer Integrated

---
+++ app/src/main/java/edu/ncuindia/healthapp/ui/home/StepListener.java
@@ -0,0 +1,12 @@
+package edu.ncuindia.healthapp.ui.home;
+
+public interface StepListener {
+
+    /**
+     * Called when a step has been detected.  Given the time in nanoseconds at
+     * which the step was detected.
+     */
+    public void step(long timeNs);
+
+}
+
